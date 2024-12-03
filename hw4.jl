205a56c7890379b515d7aef624f2b4206ecd834b
vtfanta <fantavit@fel.cvut.cz>
2024-12-03 22:32:43 +0100

    in progress 5.1

--- hw4.jl
+++ hw4.jl
@@ -1,7 +1,24 @@
 using Clarabel
 using Convex
+using GLMakie
 
-# additional exercise 5
+## exercise 5.1
+
+L(x, λ) = x^2 + 1 + λ*(x-2)*(x-4)
+lines(-1..4, x -> x^2 +1, linewidth=4)
+lines!([2,4],[0,0],color=:red)
+scatter!([2],[5],color=:green)
+
+lines!(-1..4, x -> L(x, 1))
+lines!(-1..4, x -> L(x, 2))
+lines!(-1..4, x -> L(x, 3))
+
+current_figure()
+
+g(λ) = (1+9λ-λ^2)/(1+λ)
+lines(0..4, g)
+
+## additional exercise 5
 A = [-1 0.4 0.8;
       1 0 0;
       0 1 0]
@@ -11,10 +28,3 @@ N = 30
 
 x = Variable(N+1)
 u = Variable(N)
-
-constrs = []
-for t ∈ 1:N
-    push!(constrs, x[t+1] == A*x[t] + b*u[t])
-end
-push!(constrs, x[1] .== 0)
-push!(constrs, x[N+1] == x_des)
